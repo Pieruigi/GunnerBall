6ee42e991f884244e00d66194ddd90639c9a491e
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-09-29 12:22:11 +0200

    UI wip

--- Assets/Scripts/UI/Hud/TeamScore.cs
+++ Assets/Scripts/UI/Hud/TeamScore.cs
@@ -14,7 +14,8 @@ namespace Zoca.UI
         [SerializeField]
         Text textScore;
 
-        string textFormat = "{0} : {1}";
+        //string textFormat = "{0} : {1}";
+        string textFormat = "{0}";
         bool isYou = false;
 
         // Start is called before the first frame update
@@ -40,7 +41,8 @@ namespace Zoca.UI
         // Update is called once per frame
         void Update()
         {
-            textScore.text = string.Format(textFormat, isYou ? "You" : "Opp", team == Team.Blue ? Match.Instance.BlueTeamScore : Match.Instance.RedTeamScore);
+            //textScore.text = string.Format(textFormat, isYou ? "You" : "Opp", team == Team.Blue ? Match.Instance.BlueTeamScore : Match.Instance.RedTeamScore);
+            textScore.text = string.Format(textFormat, team == Team.Blue ? Match.Instance.BlueTeamScore : Match.Instance.RedTeamScore);
         }
     }
 
